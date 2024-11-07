61b024d78ed5d04a8395c17b4a49bb9492073704
sreenathreddy025@gmail.com <sreenathreddy025@gmail.com>
2024-11-07 15:04:09 +0530

    Added images and created screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
+import data from "./data";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div >
       <header >
         <a href="/">amazona</a>
       </header>
       <main>
-        List Products
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen/>}></Route>
+          <Route path="/" element={<HomeScreen/>}></Route>
+        </Routes>
+       
+        
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
